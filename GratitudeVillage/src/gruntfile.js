4cea126b69583a56b632881b7d0394d7d5b7d673
unknown <kettljw6@IM-LT10226.Immediate.Media>
2015-05-22 13:45:12 +0100

    adding bootstrap

---
+++ GratitudeVillage/src/gruntfile.js
@@ -0,0 +1,28 @@
+﻿module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        compass: {
+            // RadioTimes
+            GratitudeBootStrap: {
+                options: {
+                    //basePath : '',
+                    sassDir: 'assets/sass',
+                    cssDir: 'assets/css',
+                    imagesDir: 'assets/images',
+                    generatedImagesDir: 'assets/images',
+                    outputStyle: 'compressed',
+                    watch: false
+                }
+            }
+        }
+    });
+
+    ////Load Task(s)
+    grunt.loadNpmTasks('grunt-contrib-compass');
+
+    ////Register Task(s).
+    grunt.registerTask('default', ['compass:GratitudeBootStrap']);
+
+};
